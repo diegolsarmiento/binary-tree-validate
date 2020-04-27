ceb52e8ffc56a6fea9072962f5a76105c14c7e87
Diego Sarmiento <44205748+diegolsarmiento@users.noreply.github.com>
2020-04-27 16:13:25 -0400

    Updated algorithm and tested it

--- src/index.js
+++ src/index.js
@@ -1,28 +1,90 @@
 class BST {
-   
-   constructor(){
-    this.value = null;
-    this.left = null;
+  constructor(value){
+    this.value = value;
     this.right = null;
+    this.left = null;
+  }
+  
+  insert(value){
+   if(value === null) {
+     return false;
+   } 
+    
+   if(value < this.value){
+    if(!this.left) {
+      this.left = new BST(value);
+    } else {
+     this.left.insert(value); 
+    }
    }
-   
-   validateTree(node, min=null, max=null){
-    if(max !== null && (node.value > max)) {
+    
+   if(value > this.value){
+    if(!this.right) {
+      this.right = new BST(value);
+    } else {
+      this.right.insert(value);
+    }
+   }
+    
+  }
+  
+  contains(value){ 
+    if (this.value === value){
+      return true;
+    }
+    
+    if(value < this.value){
+     if(!this.left){
+      return false; 
+     } else {
+      this.left.contains(value); 
+     }
+    }
+    
+    if(value > this.value){
+     if(!this.right){
+      return false; 
+     } else {
+      this.right.contains(value); 
+     }
+    }
+  }
+  
+   validateTree(min=null, max=null){
+    if(max !== null && (this.value > max)) {
     return false;
     }
     
-    if(min !== null && (node.value < min)) {
+    if(min !== null && (this.value < min)) {
     return false;
     }
     
-    if(node.left && !validateTree(node.left, min, node.value)) {
+    if(this.left && !this.left.validateTree( min, this.value)) {
     return false;
     }
     
-    if(node.right && !validateTree(node.right, node.value, max)) {
+    if(this.right && !this.right.validateTree(this.value, max)) {
     return false;
     }
     
     return true;
    }
+  
 }
+
+let bst = new BST(50);
+bst.insert(30);
+bst.insert(70);
+bst.insert(100);
+bst.insert(60);
+bst.insert(59);
+bst.insert(20);
+bst.insert(45);
+bst.insert(35);
+bst.insert(85);
+bst.insert(105);
+bst.insert(10);
+
+
+console.log(bst.validateTree(11,105));
+//console.log(bst);
